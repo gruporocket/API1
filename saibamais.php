809cb2163bd0e9e98225ad6d1cf1f696beb23072
gruporocket <79751826+gruporocket@users.noreply.github.com>
2021-05-15 15:23:31 -0300

    adicionando html e css da página "saiba mais"

---
+++ saibamais.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+    content="width=device-width, inicial-scale=1.0">
+    <title>CECOI Vó Maria Félix</title>
+    <link rel="stylesheet" type="text/css" href="style4.css">
+	<style type="text/css"> 
+    a:link 
+    { 
+     text-decoration:none; 
+    } 
+    </style>
+</head>
+<body>
+    <header>
+        <nav id="menu">
+            <ul>
+			<b>
+                <li><a href="index.php">Início</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="Formulário.php">Cadastro</a></li>
+                <li><a href="saibamais.php">Saiba mais</a></li>
+                <li><a href="#">Projetos</a></li>
+                <li><a href="comoajudar.php">Como Ajudar</a></li>
+                <li><a href="#">Contato</a></li>
+			</b>
+            </ul>
+        </nav>
+        <h1>CECOI Vó Maria Félix</h1>
+        <br>
+    </header>
+    <hr>
+    <h2>Conheça o CECOI Escola Vó Maria Félix</h2>
+        <section id="texto">
+        <!-- As informações nessa página foram copiadas da página: https://aamu.org.br/escola-vo-maria-felix/ -->
+            <p>Cerca de quarenta alunos fizeram parte do ano pioneiro. 
+            Ao longo do tempo este número foi aumentando significativamente, 
+            sendo que hoje a escola conta com cerca de cento e setenta alunos. 
+            No entanto, temos a ânsia de crescer ainda mais. 
+            Queremos aumentar e melhorar as condições do nosso estabelecimento.
+            </p>
+            <p>Já iniciamos trabalhos de ampliação, 
+            e para tal temos contado com a colaboração voluntária dos pais dos nossos alunos. 
+            Contudo, não temos recursos financeiros para realizar muitos dos nossos projetos 
+            e de melhorar ainda mais aquela que é a segunda casa das crianças. 
+            Várias são as inscrições que recebemos anualmente, cerca de 1400.
+            </p>
+            <p>Porém, por falta de possibilidades, nem todas as crianças podem fazer parte do nosso sonho. 
+            Deste modo, é feita uma seleção que tem como critérios a situação econômica e familiar. 
+            Tentamos ajudar os mais cadenciados, mas também os que podem contribuir mensalmente 
+            com o valor mínimo que a escola pede – 20 reais – para ajudar nas despesas diárias. 
+            Este valor corresponde a 10% do valor das necessidades da escola, sendo que 60% são financiados 
+            pela Prefeitura. Cerca de quatro professoras e nove educadoras preenchem o dia das nossas crianças, 
+            contribuindo para o seu desenvolvimento pessoal e cognitivo.
+            </p>
+            <p>Os nossos principais fins são: contribuir para o bem-estar, 
+            a valorização pessoal e a plena integração social das crianças e dos jovens que, 
+            por razões de natureza diversa, passam por dificuldades no seu cotidiano.
+            </p>
+            <p>Para concretizar os seus projetos, a ONG pretende promover a realização, 
+            participação ou patrocínio de ações de caráter cultural, educativo, artístico, 
+            científico, social e de assistência. Por vezes tentamos levar os nossos alunos 
+            a sair da escola em atividades que potenciam o seu desenvolvimento.
+            </p>
+            <p>Acreditamos que existe uma série de vantagens em colocar as crianças em novas 
+            situações e novos ambientes, para que elas aprendam a lidar com diferentes situações.
+            </p>
+            <p>Consideramos também que estas saídas contribuem para que a criança conheça 
+            para além da sua realidade diária e sorria com situações novas e agradáveis.
+            </p>
+            <p>Fazemos o possível para, por exemplo, levá-las à biblioteca do bairro 
+            e uma vez por ano vamos ao clube recreativo Thermas, com o qual temos uma parceria. 
+            As crianças anseiam por este dia desde o começo do ano letivo.
+            </p>
+
+        </section>
+                    <!-- Vídeo incorporado do canal no Youtube "Cecoi vó Maria Félix" -->
+        <div id="carregando" class="center" align="center">
+        <br><br><iframe width="560" height="315" src="https://www.youtube.com/embed/hB0W7N2_Ujo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen align="middle"></iframe>
+        </div>
+        <section id="info">
+            <h4>Endereço:</h4>
+            <p>Rua. Carlos Nunes de Paula, 1172 <br>
+            Prédios contíguos à Rua Dom João VI, nº 151 e 161 <br>
+            Jardim Imperial – São José dos Campos/SP. CEP: 12.234-000
+            </p>
+            <h4>Telefone:</h4>
+            <p>(12) 3966-2823</p>
+            <h4>Horário</h4>
+            <p>de Segunda a Sexta, das 07:00h às 17:00h</p>
+            <h4>Atendimento:</h4>
+            <p>67 (sessenta e sete) crianças de berçário e 123 (cento e vinte e três) crianças de infantil, 
+            totalizando 190 (cento e noventa) Crianças.</p>
+            <h4>Faixa etária atendida:</h4>
+            <p>Zero a Seis anos</p>
+            <!-- Essas informações precisam ser atualizadas pelo cliente -->
+        </section>
+        </div>
+    <hr width="100%">
+<footer>Desenvolvido por Grupo Rocket 2021</footer>
+</body>
+</html>
